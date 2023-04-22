7a19634eb90af745ea6f16c1348ef47ca2a0f229
Gaurav Kumar Yadav <gauravy60@gmail.com>
2023-04-22 20:40:22 +0530

    model trainer component

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,13 +1,16 @@
 import os
 import sys
+import glob
 
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -25,12 +28,23 @@ class DataIngestion:
         logging.info('Entered the initiate data ingestion')
         try:
             df = pd.read_csv('data/StudentsPerformance.csv')
-            logging.info('Read the dataset as df')
+            # csv_files = glob.glob(os.path.join("data", "*.csv"))
+            # df = pd.DataFrame()
 
-            os.makedirs(os.path.dirname(
-                self.ingestion_config.test_data_path), exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,
-                      index=False, header=True)
+            # for f in csv_files:
+            #     temp_df = pd.read_csv(f)
+            #     main_df = pd.concat([temp_df, df])
+
+            # logging.info('Read the dataset as df')
+            # os.makedirs(os.path.dirname(
+            #     self.ingestion_config.test_data_path), exist_ok=True)
+            # df.to_csv(self.ingestion_config.raw_data_path,
+            #           index=False, header=True)
+
+            # logging.info(
+            #     'combined all csvs into one and moved to raw data folder')
+            # for f in csv_files:
+            #     os.remove(f)
 
             logging.info('Initiating Train Test Split')
             train_set, test_set = train_test_split(
@@ -50,4 +64,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data)
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
